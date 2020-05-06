5ec404c99176e8c474c3634cb58e9edd41a53468
UserMe <aaaaasv@users.noreply.github.com>
2020-05-06 22:25:35 +0300

    all butons from create/update/other forms purple, error 404 page redesign

--- library/settings.py
+++ library/settings.py
@@ -124,21 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
-# ]
-
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # after collectstatic saves here
 STATICFILES_DIRS = ("bookshelf\\static", )
 STATIC_URL = '/static/'
+
+# STATIC_URL = '/static/' # deployment static setup
+# STATIC_ROOT = '/home/librarym/library/static'
+
 
 
 
