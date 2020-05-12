a0c71c48750b538ea86a53c11c1cc8c0b832913f
UserMe <aaaaasv@users.noreply.github.com>
2020-05-12 16:42:12 +0300

    add (R) when need to give out book to user on borrowers list page

--- bookshelf/filters.py
+++ bookshelf/filters.py
@@ -10,3 +10,9 @@ def get_status_color(status):
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter(name='has_reserved')
+def has_reserved(user_id, book):
+    u_id = int(user_id)
+    return book.paperbook.to_giveout.filter(id=u_id).count() > 0
